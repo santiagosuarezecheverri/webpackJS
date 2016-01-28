5e72ac9772ce684b51e8dd91c8ab5d00a6e72001
santiagosuarezecheverri <santiagosuarezecheverri@hotmail.com>
2016-01-28 18:03:32 -0500

    Update js2.js

--- js2.js
+++ js2.js
@@ -1,2 +1,5 @@
+//muestra mensaje 
 //document.write("js2.js ejecutado");
+
+//exporta el modulo con el mensaje que tiene adentro y llega a js1.js en la instruccion "document.write(require("./js2.js"));"
 module.exports = "module.exports = js2.js";
